3fc2d813c622e5ac41bf179e662953ef5237f2bf
mimarchant <mmarchant@moveapps.cl>
2023-06-19 21:44:16 -0400

    se agregan funciones de mostrar toast e incrementar contador

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.hellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Button toastButton = findViewById(R.id.toastBtn);
+        Button counterButton = findViewById(R.id.counterBtn);
+        TextView counter = findViewById(R.id.counter);
+
+        toastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello Toast!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        counterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                counter.setText(Integer.toString(count));
+            }
+        });
+
     }
+
+
 }
